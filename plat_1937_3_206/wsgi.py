1fb90045a453895f05e410106ab6cc2789200966
Daniel <daniel.s@crowdbotics.com>
2020-03-19 15:07:19 +0000

    Update models

--- plat_1937_3_206/wsgi.py
+++ plat_1937_3_206/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plat_1937_3_206.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "plat_1937_3_206.settings")
 
 application = get_wsgi_application()
